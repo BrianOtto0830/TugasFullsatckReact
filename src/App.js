a98a27003598a6b90a645b342311f3c9e739c9c0
BrianOtto0830 <ottobrian021@gmail.com>
2024-10-02 20:04:46 +0700

    updatecode

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
 
           - if the user is logged in :
             render a button that says logout and
-              - if the user has a username : render a greeting message with the username "Welcome username !"
+              - if the user has a username : render a greeting message with the username "Welcome username !" pake ? sebagai if : sebagai else
               - if the user does not have a user name : render "Logged and Please complete your username!""
 
         */}
