d48817fdb05519169d7ea17c0cd2bd8149e209df
BrianOtto0830 <ottobrian021@gmail.com>
2024-10-03 21:05:03 +0700

    updatetugasselesai

--- src/App.js
+++ src/App.js
@@ -9,23 +9,32 @@ export default function App() {
     <div className="App">
       <nav className="navbar">
         <Logo />
-
-        {/* Render a button that says "login" if the user is not logged in */}
-        {!user && <button>Login</button>}
-
-        {/* If the user is logged in */}
-        {user && (
-          <>
-            <button>Logout</button>
-            {/* Check if the user has a username */}
-            <p>
-              {user.username
-                ? `Welcome ${user.username}!`
-                : "Logged in, please complete your username!"}
-            </p>
-          </>
-        )}
+        {/* Render "Logout" button in the top right corner */}
+        {user && <button className="logout-btn">Logout</button>}
       </nav>
+
+      {/* Welcome message below the header */}
+      {user && (
+        <div className="welcome-message">
+          {user.username ? (
+            <p>Welcome {user.username}!</p>
+          ) : (
+            <p>Logged in, please complete your username!</p>
+          )}
+        </div>
+      )}
+
+      {/* If user is not logged in, render Login button */}
+      {!user && <button className="login-btn">Login</button>}
     </div>
   );
 }
+{/* Write your code : 
+          - if the user is not logged in : render a button that says "login" yang ini sepertinya pakai &&
+
+          - if the user is logged in :
+            render a button that says logout and
+              - if the user has a username : render a greeting message with the username "Welcome username !" pake ? sebagai if : sebagai else
+              - if the user does not have a user name : render "Logged and Please complete your username!""
+
+*/}
